1f1992ffa2680b1fb924826edc9d21ba1a932903
meustar <meustar829@gmail.com>
2025-08-18 18:35:32 +0900

    chapter04 control statement_sec04 (for)

---
+++ src/ch04/sec04/FloatCounterExample.java
@@ -0,0 +1,17 @@
+package ch04.sec04;
+
+public class FloatCounterExample {
+
+	public static void main(String[] args) {
+
+		for (float x = 0.1f; x <= 1.0f; x += 0.1f) {
+			System.out.println(x);
+		}
+
+		for (double x = 0.1; x <= 1.0; x += 0.1) {
+			System.out.println(x);
+		}
+
+	}
+
+}
